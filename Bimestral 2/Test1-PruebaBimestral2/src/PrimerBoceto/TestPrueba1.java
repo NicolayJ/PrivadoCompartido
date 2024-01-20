0ab6d9a0ac0078253bcfbe7384e9a95ace02af0b
Diego Jiménez <dnjimenez6@utpl.edu.ec>
2024-01-20 12:03:52 -0500

    Primera Prueba Terminada Solo Factura X Persona

--- Bimestral 2/Test1-PruebaBimestral2/src/PrimerBoceto/TestPrueba1.java
+++ Bimestral 2/Test1-PruebaBimestral2/src/PrimerBoceto/TestPrueba1.java
@@ -1,21 +1,20 @@
-package com.diego.main;
+package PrimerBoceto;
 import java.util.Scanner;
-public class test {
+public class TestPrueba1 {
     public static void main(String[] args) {
-        Scanner tecla = new Scanner(System.in);
-        int dia, horario, entradasNormal, entradasPrefer; 
-        double tarifaNormal, tarifaPrefe;
-        String nombre;
-        Boolean normalResp, preferResp;
+         Scanner tecla = new Scanner(System.in);
+        int dia, horario, cantidadNormal = 0, cantidadPrefer = 0;
+        String nombre, normalResp, preferResp;
         //Se pregunta al usuario 
+        System.out.println("\033[1mTexto en negrita\033[0m");
         System.out.print("Ingrese el nombre: ");
-        nombre = tecla.nextLine();
+        nombre = tecla.next();
         //Se agrega nota sobre la hora de las funciones 
         System.out.println("o---> NOTA <---o \n Las funciones tanto internacionales como nacionales empienzan en horario nocturno los precios de los boletos aumentan a partir de las 18:00 horas hasta las 02:00 horas del dia siguiente");
         //Se imprime el cronograma para que el usuario vea
         System.out.println("--------------------CRONOGRAMA-FERIA-DE-LOJA-2024--------------------\nDEL 31 DE AGOSTO HASTA EL 17 DE SEPTIEMBRE DEL 2024");
         System.out.println("====================================================================");
-        System.out.println("1. SABADO 31 DE AGOSTO \n --->GRAN APERTURA CON FUNCIONES INTERNACIONALES Y NACIONALES \n      Con la participación de : ");
+        System.out.println("1. SABADO 31 DE AGOSTO \n --->GRAN APERTURA CON FUNCIONES INTERNACIONALES Y NACIONALES \n      Con la participacion de : ");
         System.out.println("--------------------------------------------------------------------");
         System.out.println("2. DOMINGO 1 DE SEPTIEMBRE \n ---> FUNCION NACIONAL \n      Con la participacion de: ");
         System.out.println("--------------------------------------------------------------------");
@@ -49,7 +48,7 @@ public class test {
         System.out.println("--------------------------------------------------------------------");
         System.out.println("17. LUNES 16 DE SEPTIEMBRE \n ---> FUNCION INTERNACIONAL \n      Con la participacion de: ");
         System.out.println("--------------------------------------------------------------------");
-        System.out.println("18. MARTES 17 DE SEPTIEMBRE \n --->GRAN CIERRE CON FUNCIONES INTERNACIONALES Y NACIONALES \n      Con la participación de : ");
+        System.out.println("18. MARTES 17 DE SEPTIEMBRE \n --->GRAN CIERRE CON FUNCIONES INTERNACIONALES Y NACIONALES \n      Con la participacion de : ");
         System.out.println("====================================================================");
         System.out.println("o------------------------COSTO--DE--TARIFAS----------------------------o");
         System.out.println("     TARIFA NORMAL: $1,50 (Para adultos)");
@@ -58,7 +57,6 @@ public class test {
         System.out.println("___________________________________________________________________________________________________________");
         System.out.println("| Para el dia de funciones el precio de entrada aumenta a continuacion se describen los precios especiales: |");
         System.out.println("-----------------------------------------------------------------------------------------------------------");
-        System.out.println("-------------------------------------------------------------------------");
         System.out.println("FUNCION NACIONAL <<-----");
         System.out.println("     TARIFA NORMAL: $5,00 (Para adultos)");
         System.out.println("     TARIFA PREFERENCIAL: $2,50 (Para personas con discapacidad y tercera edad)");
@@ -69,23 +67,39 @@ public class test {
         System.out.println("     TARIFA NORMAL: $10,0 (Para adultos)");
         System.out.println("     TARIFA PREFERENCIAL: $5,00 (Para personas con discapacidad y tercera edad)");
         // Se pide el dia de asistencia
-        System.out.println("Ingrese el dia al cual desea asistir(debe ingresar el numeral asignado en el cronograma): ");
+        System.out.println("Ingrese el dia al cual desea asistir (debe ingresar el numeral asignado en el cronograma): ");
         dia = tecla.nextInt();
         // Se escoge el horario de dia o de noche
-        System.out.println("Ingrese el horario el cual desea ingresar (ingrese el numeral del horario a escoger): \n 1. Horario Normal (Empieza a partir de las 02:01 am hasta las 17:59 pm) \n 2. Horario Nocturno  (Empiez a partir de las 18:00 pm hasta las 02:00 am)");
+         System.out.println("Ingrese el horario el cual desea ingresar (ingrese el numeral del horario a escoger): \n 1. Horario Normal (Empieza a partir de las 02:01 am hasta las 17:59 pm) \n 2. Horario Nocturno  (Empieza a partir de las 18:00 pm hasta las 02:00 am)");
         horario = tecla.nextInt();
         // Preguntar al usuario si desea comprar entradas normales y preferenciales
-        System.out.print("Desea comprar entradas de tarifa Normal (si/no)? "); 
-        normalResp = tecla.nextBoolean();
-        if (normalResp) { System.out.print("Ingrese la cantidad de entradas de tarifa Normal que desea comprar: "); 
-        entradasNormal = tecla.nextInt(); }
-        System.out.print("Desea comprar entradas de tarifa Preferencial (si/no)? "); 
-        preferResp = tecla.nextBoolean();
-        if (preferResp) { System.out.print("Ingrese la cantidad de entradas de tarifa Preferencial que desea comprar: "); 
-        entradasPrefer = tecla.nextInt(); }
+         System.out.print("Desea comprar entradas de tarifa Normal (si/no)? ");
+        normalResp = tecla.next();
+        if (normalResp.equals("si") || normalResp.equals("Si")) {
+            System.out.println("Cuantas entradas de tarifa normal desea comprar?: ");
+            cantidadNormal = tecla.nextInt();
+        }
+        System.out.print("Desea comprar entradas de Tarifa Preferencial (si/no): ");
+        preferResp = tecla.next();
+        if (preferResp.equals("si") || preferResp.equals("Si")) {
+            System.out.println("Cuantas entradas de tarifa preferencial desea comprar?: ");
+            cantidadPrefer = tecla.nextInt();
+        }
+
+        double costoTarifaNormal = costTariNor(dia, horario);
+        double costoTarifaPreferencial = costTariPrefe(dia, horario);
+
+        System.out.println("o----------FACTURA---------------o");
+        System.out.println("El costo de la tarifa normal escogida por persona es de: $" + costoTarifaNormal);
+        System.out.println("Cantidad adquirida: " + cantidadNormal + " y el costo total de las entradas normales es de: $" + sumaTarNormal(costoTarifaNormal, cantidadNormal));
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("El costo de la tarifa preferencial escogida por persona es de: $" + costoTarifaPreferencial);
+        System.out.println("Cantidad adquirida: " + cantidadPrefer + " y el costo total de las entradas preferenciales es de: $" + sumaTarPrefer(costoTarifaPreferencial, cantidadPrefer));
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("El cliente "+ nombre +" debe cancelar un costo total de las entradas de: $" + sumaTotalTariInd(sumaTarNormal(costoTarifaNormal, cantidadNormal), sumaTarPrefer(costoTarifaPreferencial, cantidadPrefer)));
     }
     // Calcular la tarifa normal en base al dia escogido
-    public double costTariNor(int dia,int horario, boolean normalResp){
+    public static double costTariNor(int dia, int horario){
         double tarifaNormal;
         if (dia == 1 || dia == 18) { // Dia de Apertura y Cierre
             if (horario == 2) {
@@ -114,7 +128,7 @@ public class test {
         }
         return tarifaNormal;
     }
-    public double costTariPrefe(int dia,int horario, boolean preferResp){
+    public static double costTariPrefe(int dia, int horario){
         double tarifaPrefe;
         if (dia == 1 || dia == 18) { // Dia Apertura y de Cierre
             if (horario == 2) {
@@ -143,5 +157,19 @@ public class test {
         }
         return tarifaPrefe;
     }
-
+    public static double sumaTarNormal(double tarifaNormal, int cantidadNormal) {
+        double sumaTarNormal;
+        sumaTarNormal = tarifaNormal * cantidadNormal;
+        return sumaTarNormal;
+    }
+    public static double sumaTarPrefer (double tarifaPrefe, int cantidadPrefer){
+        double sumaTarPrefer;
+        sumaTarPrefer = tarifaPrefe * cantidadPrefer; 
+        return sumaTarPrefer;
+    }
+    public static double sumaTotalTariInd (double sumaTarNormal, double sumaTarPrefer){
+        double sumaTotalTariInd;
+        sumaTotalTariInd = sumaTarNormal + sumaTarPrefer;
+        return sumaTotalTariInd;
+    }
 }
